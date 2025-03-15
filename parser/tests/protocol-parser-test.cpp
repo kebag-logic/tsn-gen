76fd30f9d2f4824c6942eb00240c36725f24cc1b
Alex. M <alexandremalki89@gmail.com>
2025-03-15 17:15:41 +0100

    parser: tests: adding new test: BasePath

--- parser/tests/protocol-parser-test.cpp
+++ parser/tests/protocol-parser-test.cpp
@@ -3,9 +3,14 @@
 
 
 TEST(ParserUsingTest, Version) {
-	ProtocolParser testProtocol; 
+	ProtocolParser testProtocol("NOTHING");
 
 	EXPECT_STREQ(PROTOCOL_PARSER_VERSION, testProtocol.getVersion().c_str());
 }
 
+TEST(ParserUsingTest, BasePath) {
+	const std::string dummypath("NOTHING");
+	ProtocolParser testProtocol(dummypath);
 
+	EXPECT_STREQ(dummypath.c_str(), testProtocol.getBasePath().c_str());
+}
