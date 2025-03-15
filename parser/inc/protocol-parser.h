09d983f19c9dab3e6d3c96b573f3b442ca0c9368
Alex. M <alexandremalki89@gmail.com>
2025-03-15 17:13:03 +0100

    parser: adding base path

--- parser/inc/protocol-parser.h
+++ parser/inc/protocol-parser.h
@@ -5,8 +5,12 @@
 
 class ProtocolParser {
 public:
-	ProtocolParser();
-	~ProtocolParser() = default;
+	ProtocolParser(const std::string& baseProtoPath);
+	/**
+	 * Get rid of the default copy constructors
+	 */
+	ProtocolParser(const ProtocolParser &) = delete;
+	ProtocolParser& operator=(const ProtocolParser &) = delete;
 
 	/**
 	 * @brief Provide the Version of the parser
@@ -14,6 +18,15 @@ public:
 	 */
 	const std::string& getVersion(void) const;
 
+	/**
+	 * @brief Provide the base path (top level directory)
+	 * 	Where are located all the protocols
+	 * @return a reference to the base path where the
+	 * 	protocol are  specified
+	 */
+	const std::string& getBasePath(void) const;
+
 private:
 	const std::string mProtocolParserVersion;
+	const std::string mProtocolBasePath;
 };
--- parser/src/protocol-parser.cpp
+++ parser/src/protocol-parser.cpp
@@ -3,12 +3,18 @@
 #include <protocol-parser.h>
 #include <version.h>
 
-ProtocolParser::ProtocolParser(void):
-	mProtocolParserVersion(PROTOCOL_PARSER_VERSION)
+ProtocolParser::ProtocolParser(const std::string& baseProtoPath):
+	mProtocolParserVersion(PROTOCOL_PARSER_VERSION),
+	mProtocolBasePath(baseProtoPath)
 {
 }
 
 const std::string& ProtocolParser::getVersion(void) const
 {
 	return mProtocolParserVersion;
+}
+
+const std::string& ProtocolParser::getBasePath(void) const
+{
+	return mProtocolBasePath;
 }
