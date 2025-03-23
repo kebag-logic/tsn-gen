367a010bdb6de39c03cc08f62c176d74eee4ef82
Alex. M <alexandremalki89@gmail.com>
2025-03-23 16:37:22 +0100

    tests: make the test-on-change project based

--- tests/run-tests-on-changes.sh
+++ tests/run-tests-on-changes.sh
@@ -3,10 +3,12 @@
 SCRIPT=$(realpath -s "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+PROJECT=${1}
+
 while true
 do
 	clear
-	inotifywait -qrm --event modify --format '%w%f' ${SCRIPTPATH}/../parser/ | 
+	inotifywait -qrm --event modify --format '%w%f' ${SCRIPTPATH}/../${PROJECT}/ |
 	while read -r file ; do
 		echo "Changes in $file"
 		${SCRIPTPATH}/../setup.sh
